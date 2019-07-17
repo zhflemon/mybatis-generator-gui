2a91043f16938d4402eb1a83fe998881359caf36
zuohaifeng <zhflemon@126.com>
2019-07-17 14:42:45 +0800

    代码格式化

--- src/main/java/com/zzg/mybatis/generator/MainUI.java
+++ src/main/java/com/zzg/mybatis/generator/MainUI.java
@@ -1,24 +1,30 @@
 package com.zzg.mybatis.generator;
 
+import java.net.URL;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.zzg.mybatis.generator.controller.MainUIController;
 import com.zzg.mybatis.generator.util.ConfigHelper;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.swing.*;
-import java.net.URL;
 
 /**
  * 这是本软件的主入口,要运行本软件请直接运行本类就可以了,不用传入任何参数
  */
+@SuppressWarnings("restriction")
 public class MainUI extends Application {
 
+	@SuppressWarnings("unused")
 	private static final Logger _LOG = LoggerFactory.getLogger(MainUI.class);
 
 	@Override
@@ -37,9 +43,10 @@ public class MainUI extends Application {
 
 	public static void main(String[] args) {
 		String version = System.getProperty("java.version");
-		if (Integer.parseInt(version.substring(0,1)) == 1 && Integer.parseInt(version.substring(2, 3)) >= 8 && Integer.parseInt(version.substring(6)) >= 60 || Integer.parseInt(version.substring(0,1))>=9) {
+		if (Integer.parseInt(version.substring(0, 1)) == 1 && Integer.parseInt(version.substring(2, 3)) >= 8
+				&& Integer.parseInt(version.substring(6)) >= 60 || Integer.parseInt(version.substring(0, 1)) >= 9) {
 			launch(args);
-		}else {
+		} else {
 			JFrame jFrame = new JFrame("版本错误");
 			jFrame.setSize(500, 100);
 			jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
